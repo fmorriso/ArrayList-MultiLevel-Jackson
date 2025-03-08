f2e2263a43a0060007263543c38a71c57551c4c4
Fred Morrison <frederick.morrison@outlook.com>
2025-03-07 17:06:04 -0700

    document example call to getPOJOfromJson

--- src/PersistenceHandler.java
+++ src/PersistenceHandler.java
@@ -3,8 +3,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-import java.lang.reflect.InvocationTargetException;
-
 public class PersistenceHandler {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
